a1862cda445aabdd44f337389e340dc7d1945d84
Leggera <lana.krlv1995@gmail.com>
2017-05-10 01:11:29 +0300

    Update pretrain.sh

--- pretrain.sh
+++ pretrain.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+git clone https://github.com/mesnilgr/iclr15.git
+cp iclr15/scripts/word2vec.c .
+gcc word2vec.c -o word2vec -lm -pthread -O3 -march=native -funroll-loops
+
 function normalize_text {
   awk '{print tolower($0);}' < $1 | sed -e 's/\./ \. /g' -e 's/<br \/>/ /g' -e 's/"/ " /g' \
   -e 's/,/ , /g' -e 's/(/ ( /g' -e 's/)/ ) /g' -e 's/\!/ \! /g' -e 's/\?/ \? /g' \
